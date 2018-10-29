2a11fe7ba956f6ff3dc5cc880a27b852a5951c38
Lucas Gomes <lucasgsm88@gmail.com>
2018-10-29 20:25:29 -0300

    Add files via upload

---
+++ GokuTests/CriarObjetosTeste.cs
@@ -0,0 +1,25 @@
+﻿using Goku;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GokuTests
+{
+    public class CriarObjetosTeste
+    {
+        [TestMethod()]
+        public void LerArquivoTeste1()
+        {
+            Arquivo arq = new Arquivo();
+            try
+            {
+                Arquivo.CriarObjetos(null);
+                Assert.IsTrue(true);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+    }
+}
